fadf107eb500c5a36b54565688b59ab22237f693
WildRider07 <88147105+WildRider07@users.noreply.github.com>
2022-12-20 18:16:47 +0200

    Python file with sort trash script

---
+++ GOIT_DZ6/DZ_6.py
@@ -0,0 +1,88 @@
+import shutil
+from pathlib import Path
+import sys
+trash = r'C:\Users\User\Documents\Python\trash'
+unknown_ext = set()
+known_ext = set()
+x = sys.argv[0]
+files_by_ext = {'images':[],'video':[],'documents':[],'music':[],'archives':[]}
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+            "f", "h", "ts", "ch", "sh", "sch", "", "y", "'", "e", "yu", "ya", "je", "i", "ji", "g")
+TRANS = {}
+for i, j in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(i)] = j
+    TRANS[ord(i.upper())] = j.upper()
+def sort_trash(folder):
+    global unknown_ext,known_ext,trash
+    target = Path(folder)
+    for f in target.iterdir():
+        ext = f.suffix
+        pref = f.name.removesuffix(ext)
+        transF = normalize(pref)
+        newF = trash+'/'+transF+ext
+        f.rename(newF)
+    for f in target.iterdir():
+        if f.is_dir():
+            if f.name.casefold() in 'archives, video, audio, documents, images':
+                continue            
+            elif not any(f.iterdir()):
+                f.rmdir()
+                continue
+            else:
+                sort_trash(f)
+        else:
+            ext = f.suffix
+            ext = ext.upper()
+            dst =''
+            if ext in ['.ZIP', '.GZ', '.TAR','.RAR', '.7Z']:
+                dst = 'archives'
+                known_ext.add(ext)
+                files_by_ext[dst].append(f.name)
+                dot = f.name.index('.')
+                subfolder = Path(f.name[:dot])
+                dst_dir = Path(f'{trash}/{dst}')
+                subfolder.mkdir(parents=True, exist_ok=True)
+                dst_dir.mkdir(parents=True, exist_ok=True)
+                shutil.unpack_archive(f,subfolder)
+                shutil.move(subfolder,dst_dir)
+                f.unlink(missing_ok=True)
+            else:
+                if ext in ['.JPEG', '.PNG', '.JPG', '.SVG']:
+                    dst = 'images'                    
+                elif ext in ['.AVI', '.MP4', '.MOV', '.MKV']:
+                    dst = 'video'
+                elif ext in ['.DOC', '.DOCX', '.TXT', '.PDF', '.XLS','.XLSX', '.PPTX']:
+                    dst = 'documents'
+                elif ext in ['.MP3', '.OGG', '.WAV', '.AMR','.M4A']:
+                    dst = 'music'
+                else:
+                    unknown_ext.add(ext)
+                    continue
+                known_ext.add(ext)
+                dst_dir = Path(f'{trash}/{dst}')
+                dst_dir.mkdir(parents=True, exist_ok=True)
+                files_by_ext[dst].append(f.name)
+                shutil.move(f,dst_dir)
+
+    # sort_trash(f)
+
+                # if not any(f.parent.iterdir()):
+                #     f.parent.rmdir()
+                #     continue
+                
+    return files_by_ext,known_ext,unknown_ext
+
+def normalize(name):
+    global TRANS
+    translit = name.translate(TRANS)
+    for letter in translit:
+        if ord(letter) in range (65, 91) or ord(letter) in range (97, 123) or ord(letter) in range (48, 58):
+            continue
+        else:
+            translit = translit.replace(letter,'_')
+    return translit
+
+
+print(sort_trash(trash))
+# sort_trash(x)
