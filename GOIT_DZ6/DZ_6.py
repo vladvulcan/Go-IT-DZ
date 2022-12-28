ed4470870762b6c39d67be915f43779cf01ba383
WildRider07 <sidvulk@gmail.com>
2022-12-28 18:52:31 +0200

    Updated the code according to the comments

--- GOIT_DZ6/DZ_6.py
+++ GOIT_DZ6/DZ_6.py
@@ -1,10 +1,9 @@
 import shutil
 from pathlib import Path
 import sys
-trash = r'C:\Users\User\Documents\Python\trash'
 unknown_ext = set()
 known_ext = set()
-x = sys.argv[0]
+x = sys.argv[1]
 files_by_ext = {'images':[],'video':[],'documents':[],'music':[],'archives':[]}
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
@@ -14,18 +13,19 @@ for i, j in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(i)] = j
     TRANS[ord(i.upper())] = j.upper()
 def sort_trash(folder):
-    global unknown_ext,known_ext,trash
+    global unknown_ext,known_ext
     target = Path(folder)
     for f in target.iterdir():
         ext = f.suffix
         pref = f.name.removesuffix(ext)
         transF = normalize(pref)
-        newF = trash+'/'+transF+ext
+        newF = target.joinpath(folder,transF+ext)
         f.rename(newF)
+
     for f in target.iterdir():
         if f.is_dir():
             if f.name.casefold() in 'archives, video, audio, documents, images':
-                continue            
+                continue
             elif not any(f.iterdir()):
                 f.rmdir()
                 continue
@@ -41,7 +41,7 @@ def sort_trash(folder):
                 files_by_ext[dst].append(f.name)
                 dot = f.name.index('.')
                 subfolder = Path(f.name[:dot])
-                dst_dir = Path(f'{trash}/{dst}')
+                dst_dir = target.joinpath(subfolder,dst)
                 subfolder.mkdir(parents=True, exist_ok=True)
                 dst_dir.mkdir(parents=True, exist_ok=True)
                 shutil.unpack_archive(f,subfolder)
@@ -49,7 +49,7 @@ def sort_trash(folder):
                 f.unlink(missing_ok=True)
             else:
                 if ext in ['.JPEG', '.PNG', '.JPG', '.SVG']:
-                    dst = 'images'                    
+                    dst = 'images'
                 elif ext in ['.AVI', '.MP4', '.MOV', '.MKV']:
                     dst = 'video'
                 elif ext in ['.DOC', '.DOCX', '.TXT', '.PDF', '.XLS','.XLSX', '.PPTX']:
@@ -60,17 +60,12 @@ def sort_trash(folder):
                     unknown_ext.add(ext)
                     continue
                 known_ext.add(ext)
-                dst_dir = Path(f'{trash}/{dst}')
+                dst_dir = target.joinpath(folder,dst)
                 dst_dir.mkdir(parents=True, exist_ok=True)
                 files_by_ext[dst].append(f.name)
                 shutil.move(f,dst_dir)
 
-    # sort_trash(f)
 
-                # if not any(f.parent.iterdir()):
-                #     f.parent.rmdir()
-                #     continue
-                
     return files_by_ext,known_ext,unknown_ext
 
 def normalize(name):
