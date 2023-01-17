3ebfce1bfeb2c0942239dc93fca673e971195e48
WildRider07 <sidvulk@gmail.com>
2023-01-17 13:46:22 +0200

    Update setup.py

--- GOIT_DZ7/clean_folder/setup.py
+++ GOIT_DZ7/clean_folder/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup (
     name = 'sort_trash',
     version = 1.0,
+    description='Этот скрипт сделан в ДЗ №6 и сортирует файлы и папки по категориям',
     author='Wildrider07',
     packages= find_namespace_packages(),
     entry_points = {'console_scripts': ['clean-folder = clean_folder.clean:main']}
