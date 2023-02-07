433df1c0a197518c937f8ff7a400166730847e66
WildRider07 <sidvulk@gmail.com>
2023-02-07 14:58:58 +0200

    Bot v 2.0
    Added more currying, split main further more into small functions, removed prints from the decorator

--- GOIT_DZ9/DZ9.py
+++ GOIT_DZ9/DZ9.py
@@ -1,67 +1,34 @@
 memory = {}
 
 def input_error(func):
-    def inner(command):
+    def inner(*args, **kwargs):
         try:
-            res = func(command)
+            res = func(*args, **kwargs)
             return res
 
         except KeyError:
-            print ("No such name")
+            return "No such name"
         
-        except ValueError:
-            print ('Invalid value')
+        except ValueError as err:
+            return err
 
         except IndexError:
-            print ('Name is given, but phone number is not')
+            return 'Name is given, but phone number is not'
+        
+        except TypeError:
+            return "Give me name and phone please"
 
     return inner
 
+@input_error
+def say_hello():
+    return 'How can I help you?'
 
-def main():
-    while True:
-        command = input('enter command: ')
-        command = command.lower()
+def say_goodbye():
+    return 'Good bye!'
 
-        if command == '.':
-            break
-
-        elif command == 'hello':
-            print ('How can I help you?')
-
-        elif command.startswith('add'):            
-            add_command = command.removeprefix('add')
-            if len(add_command)>0:
-                add(add_command)
-            else:
-                print ("Give me name and phone please")
-            
-        
-        elif command.startswith('change'):
-            change_command = command.removeprefix('change')
-            if len(change_command)>0:
-                change(change_command)
-            else:
-                print ("Give me name and phone please")
-            
-        
-        elif command.startswith('phone'):
-            search_command = command.removeprefix('phone')
-            if len(search_command)>0:
-                search(search_command)
-            else:
-                print("Enter user name")
-
-        elif command == "show all":
-            for key, value in memory.items():
-                print (f'{key}: {value}')
-
-        elif command in ['goodbye', 'close', 'exit']:
-            print ("Good bye!")
-            break
-
-        elif command == 'help':
-            help = '''
+def show_help():
+    help = '''
 Список доступных команд:
 "hello" - бот отвечает "How can I help you?"
 "add ..." -   бот сохраняет в памяти новый контакт. Вместо ... введите имя и номер телефона через пробел.
@@ -69,37 +36,87 @@ def main():
 "phone ..." - бот выводит в консоль номер телефона для указанного контакта. Вместо ... введите имя пользователя через пробел.
 "show all" - бот выводит все сохраненные контакты с номерами телефонов в консоль в формате "Имя: телефон".
 "goodbye", "close", "exit" - бот пишет "Good bye!" и завершает свою работу.
-            '''
-            print (help)
-
-        else:
-            print ('''Invalid command. Try again.
-For the list of available commands type "help"
-            ''')
-            continue
+'''
+    return help
 
 @input_error
-def add(command: str):    
-    global memory
-    name_phone = command.split()
-    name, phone_number = name_phone[0], name_phone[1]        
-    memory[name] = phone_number
-    print (f'Added to memory: Name - {name}, phone - {phone_number}')
+def add_new_user(command: str):
+    name_phone = command[1:].split()
+    name, phone_number = name_phone[0], name_phone[1]
+    if not name.isalpha():
+        raise ValueError('Name must be a text, not number')
+    if not phone_number.isdecimal():
+        raise ValueError('Numbers-only phones are allowed')
+    if name in memory:
+        raise ValueError(f'This name already exists.\nType "phone {name}" to see the number linked to it.')
+    else: memory[name] = phone_number
+    message = f'Added to memory: Name - {name}, phone - {phone_number}'
+    return message
     
 @input_error
-def change(command: str):
-    global memory
-    name_phone = command.split()
-    name, new_phone_number = name_phone[0], name_phone[1]    
-    old_number = memory[name]
+def change_existing_users_phone(command: str):
+    name_phone = command[1:].split()
+    name, new_phone_number = name_phone[0], name_phone[1]
+    if not new_phone_number.isdecimal():
+        raise ValueError('Numbers-only phones are allowed')    
+    if not name in memory:
+        raise KeyError
     memory[name] = new_phone_number
-    print (f'Changed in memory: Name - {name}, new phone - {new_phone_number}')
+    message = f'Changed in memory: Name - {name}, new phone - {new_phone_number}'
+    return message
 
 @input_error
-def search(command: str):
-    global memory
-    name = command.removeprefix(' ')
-    print (memory.get(name,"No such name"))
+def get_users_phone(command: str):
+    name = command[1:]
+    return memory[name]
+
+@input_error
+def get_database():
+    contacts = ''
+    for key, value in memory.items():
+         contacts += f'{key} : {value} \n'
+    return contacts
+
+
+
+COMMANDS_DICT = {
+    'help': show_help,
+    'hello': say_hello,
+    'exit': say_goodbye,
+    'close': say_goodbye,
+    'good bye': say_goodbye,
+    'add': add_new_user,
+    'change': change_existing_users_phone,
+    'phone': get_users_phone,
+    'show all': get_database
+}
+
+
+def command_selector(command):
+    return COMMANDS_DICT[command]
+
+def parser(user_input: str):
+    for key in COMMANDS_DICT:
+        if user_input.lower().startswith(key):
+            action = command_selector(key)
+            data = user_input.removeprefix(key)
+            if data:
+                return action(data)
+            else:
+                return action()
+    error_message = 'Invalid command. Try again.\nFor the list of available commands type "help"'
+    return error_message
+                
+
+def main():
+    while True:
+        user_input = input('enter command: ')
+        if user_input == '.':
+            break
+        result = parser(user_input)
+        print (result)
+        if result == 'Good bye!':
+            break        
     
 
 
